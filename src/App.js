7a13d705bf0f50577db75bf69f03792c2f9ea7d2
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-20 20:23:38 +0200

    implementation Create Post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }   from 'react';
+import React, { useState, useEffect, Fragment }   from 'react';
 import Login from './components/Login'
 import Header from './components/Header'
 import CreatePost from './components/CreatePost'
@@ -6,6 +6,7 @@ import CreatePost from './components/CreatePost'
 
 function App() {
     const [user, setUser] = useState('eed');
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         document.title= user ? `${user}'s Feed`: 'Please login';
@@ -17,9 +18,22 @@ function App() {
     return (
         <>
         <Header user={user} setUser={setUser}/>
-        <CreatePost  user={user} />
+        <CreatePost  user={user}  setPosts={setPosts} posts={posts}/>
+        {posts.map((post, i) => (
+            <Fragment kye ={i}>
+            {post.image && (
+                <img 
+                style={{height: 100, width: 200, objectFit: 'cover'}}
+                src={URL.createObjectURL(post.image)}
+                alt="Post cover"
+                />
+            )}
+            <p>{post.content}</p>
+            <div>{user}</div>
+            </Fragment>
+            ))}
         </>
-    )
+    );
 }
 
 export default App
